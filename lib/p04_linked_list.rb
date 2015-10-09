476d31afbc5a96aa63b16da4d0537b40880a2677
Ben&Ryan <Ben&Ryan@appacademy.io>
2015-10-09 18:25:51 -0400

    finished part 4

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,8 +1,10 @@
+require "byebug"
 class Link
-  attr_accessor :key, :val, :next
+
+  attr_accessor :key, :val, :nxt
 
   def initialize(key = nil, val = nil, nxt = nil)
-    @key, @val, @next = key, val, nxt
+    @key, @val, @nxt = key, val, nxt
   end
 
   def to_s
@@ -11,6 +13,9 @@ class Link
 end
 
 class LinkedList
+
+  include Enumerable
+
   attr_reader :head
 
   def initialize
@@ -23,31 +28,93 @@ class LinkedList
   end
 
   def first
+    @head.nxt
   end
 
-  def last
+  def last(link)
+    if link.nxt.nil?
+      return link
+    else
+      last(link.nxt)
+    end
   end
 
   def empty?
+    self.empty?
   end
 
   def get(key)
+    if @head.nxt.key == key
+      return @head.nxt.val
+    else
+    return get_recurse(@head.nxt,key)
+    end
   end
+
+  def get_recurse(link,key)
+    return nil if link.nil?
+    if link.key == key
+      return link.val
+    else
+      get_recurse(link.nxt,key)
+    end
+  end
+
 
   def include?(key)
+    !get_recurse(@head,key).nil?
+
   end
 
+
   def insert(key, val)
+    a = Link.new(key, val, @head.nxt)
+    @head.nxt = a
   end
 
   def remove(key)
+    if @head.nxt != nil || @head.nxt.key == key
+      @head.nxt = @head.nxt.nxt
+    end
+
+    found_link = remove_recurse(@head, key)
+    found_link.nxt = found_link.nxt.nxt
+
   end
 
+  def remove_recurse(link,key)
+    return nil if link.nil?
+    if link.nxt.key == key
+      return link
+    else
+      remove_recurse(link.nxt,key)
+    end
+  end
+
+
+
+
   def each
+    link = @head.nxt
+    array = []
+    # until link.nxt.nil?
+    #   yield(link)
+    #   link = link.nxt
+    # end
+    stupid_rec(link,array)
+    array.each do |link|
+      yield link
+    end
+  end
+
+  def stupid_rec(link,array)
+    return nil if link.nil?
+    stupid_rec(link.nxt,array)
+    array = array << link
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
